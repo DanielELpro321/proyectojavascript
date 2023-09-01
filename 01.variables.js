6af9b2c945de40615f1cef96fb54f89fa3a6af62
DanielELpro321 <dm3699492@gmail.com>
2023-08-31 23:21:25 -0500

    subir todos los archivos

---
+++ 01.variables.js
@@ -0,0 +1,50 @@
+//variables= guardar un espacio en memoria
+// los tres variabes var, let y const
+let mascota;
+
+mascota="Perro";
+
+console.log(mascota)
+
+//REGLAS DE LAS VARIABLES
+/*1. Let no me permite declarar variable, con el mismo nombre
+2.let permite declarar la variables y no inicializarla y mas en cualquier lugar del codigo iniciarla
+3.var me permite declarar otra variables con el mismo nobre 
+4.tambien me permite declarar sin inicializar
+5. const no nos deja declarar una variable sin inicializarla
+6. const no nos deja declarar una variable sin el mismo nombre
+*/ 
+
+var nombre="pedro"
+
+console.log(nombre)
+ 
+var nombre = "carlos"
+
+console.log(nombre)
+
+/*
+variabe undefined o variable null
+*/
+
+let pez;
+
+console.log(pez)
+
+// function saltar(){
+//     let tiburon = "blanco"
+// }
+
+// console.log(tiburon)
+
+//estado inicial
+
+// var a = 1; 
+
+// var b = 2;
+
+// console.log( a + b )
+
+//nuca usaremos var 
+//usaremos let cuando nuestra variable valla a cambiar 
+//usaermo const cuando nuestra variable sera fijo
---
+++ 02.TipoDeDaros.js
@@ -0,0 +1,10 @@
+//Objetos (objetos y arraylist)
+const personas={
+    nombre:"Daniel",
+    apellidos: "Miranda",
+    edad: 32
+}
+//console.log(personas)
+//arraylist y listas 
+const frutas = ['pera','limon','Manzana']
+//primitivos: number, floats, Strings, boolean, undefined,null
